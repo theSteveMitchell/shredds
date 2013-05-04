c7a3113e94f29bc679935ae178828d6cdd707c6f
Steve Mitchell <theSteveMitchell@gmail.com>
2013-05-03 18:19:10 -0600

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,32 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require smoothDivScroll/jquery-ui-1.8.23.custom.min
+//= require smoothDivScroll/jquery.kinetic
+//= require smoothDivScroll/jquery.mousewheel.min
+//= require smoothDivScroll/jquery.smoothdivscroll-1.3-min
+
+
+
+//
+//$(document).ready(function () {
+//    $("#scroll-area").smoothDivScroll({
+//        mousewheelScrolling: "allDirections",
+//        autoScrollingMode: "onStart",
+//        scrollingHotSpotLeftClass: "scrollingHotSpotLeft",
+//        scrollingHotSpotRightClass: "scrollingHotSpotRight",
+//        visibleHotSpotBackgrounds: "always"
+//    });
+//});
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedding::Application.config.secret_token = '2994895927da9f8b6de63a7b51a3fe94266349e1ccb2ba9fa6e9930792d68d7f9e9b5704187048a65b32d4fef2d7b64c35791f8eca1f900c6a4798757001e75f'
